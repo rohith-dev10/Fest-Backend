baf04be7f409ab95225253861a084207fa6ef806
M Rohith <developer.mr101804@gmail.com>
2023-01-09 16:09:49 +0530

    first

---
+++ teamModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const TeamSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    emails: {
+        type: [{
+            email: {
+                type:String,
+                required:true
+            }
+        }],
+        required: true,
+    },
+    leader:{
+        type:String,
+        required:true
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Team', TeamSchema)
