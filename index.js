6776280db51a9064d1ad0fd650f1cefc66756c41
M Rohith <developer.mr101804@gmail.com>
2023-01-14 02:16:50 +0530

    Backend done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express    = require('express');
+const cors    = require('cors');
 const router     = require('./src/routes/route.js');
 const authrouter = require('./src/routes/Routes.js');
 const mongoose   = require('mongoose');
 
 mongoose.set('strictQuery', true);
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect("mongodb+srv://rohith:wb5T5JFful4l5YVB@cluster0.bpry8kq.mongodb.net/Fest-Website?retryWrites=true&w=majority", {
